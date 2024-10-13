611e49d584951a412bdc85728d50ba0346c00148
1ynxa <tktk.kanako925@gmail.com>
2024-10-13 15:46:39 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
+  );
+}
+
+export default App;
