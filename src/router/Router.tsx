c21cc16d31aea0214cac08091d2283f667a87e43
1ynxa <tktk.kanako925@gmail.com>
2024-10-13 23:44:36 +0900

    ヘッダー周りのコンポーネント分割

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,16 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home">
+
+      <Route path="/home" element={<HeaderLayout />}>
         {homeRoutes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
+        <Route path="*" element={<Page404 />} />
       </Route>
+
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
